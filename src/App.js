5ec443b6cac3ca1bc3ac0767438fb50aecb844e6
Iurii Lopatenko <yalopatenko@gmail.com>
2020-12-26 19:54:17 -0800

    Section 3. Lesson 7. App component should return just one parent div with all the stuff inside it

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 class App extends Component {
   render() {
     
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I'm a react app</h1>
-    //   </div>
-    // );
+    return (
+      <div className="App">
+        <h1>Hi, I'm a react app</h1>
+        <p>It is really working!</p>
+      </div>
+    );
 
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null,'Hi, I\'m a react app with non jsx code style'));
+    // Section 3. Lesson 6. Using non JSX code style to create header h1 inside div
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null,'Hi, I\'m a react app with non jsx code style'));
 
   }
 } 
