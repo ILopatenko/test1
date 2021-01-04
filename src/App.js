e099ff541f78913c3ebf22da62f8ee8925141482
Iurii Lopatenko <yalopatenko@gmail.com>
2021-01-04 15:36:51 -0800

    Lesson 3.20 – Using the useState() HOOK for STATE manipulating.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,41 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import Person from './Person/Person'
-class App extends Component {
-  state = {
+import Person from './Person/Person';
+const app = (props) => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       {name: 'Karl1', age: 25},
       {name: 'John1', age: 19},
       {name: 'Kate1', age: 35},
       {name: 'Jane1', age: 70},
-    ],
-    otherState: 'Some value for otherState'
-  };
-  switchNameHandler = () => {
-    //console.log('switchNameHandler is working');
-    //DON'T DO THIS: this.state.persons[0].`name = 'Iurii';
-    this.setState({
+    ]
+  });
+  const [otherState, setState2] = useState({id: '33--02'});
+  console.log(personsState);
+  console.log(otherState);
+
+  const switchNameHandler = () => {
+    setPersonsState({
       persons: [
         {name: 'Iurii', age: 40},
         {name: 'Stepan', age: 37},
         {name: 'Kate1', age: 35},
         {name: 'Jane1', age: 70},
       ]
-    })
+    });
   };
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a new React app</h1>
-        <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch names</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}> My hoobies: Racing </Person>
-        <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
-      </div>
-    );
-  }
+return (
+  <div className="App">
+    <h1>Hi, I'm a new React app</h1>
+    <p>This is really working!</p>
+    <button onClick={switchNameHandler}>Switch names</button>
+    <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+    <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
+    <Person name={personsState.persons[2].name} age={personsState.persons[2].age}> My hoobies: Racing </Person>
+    <Person name={personsState.persons[3].name} age={personsState.persons[3].age}/>
+    </div>
+  );
 }
-export default App;
+export default app;
+
+
